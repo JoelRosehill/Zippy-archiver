8b5a93b62f6bc36b53f76b6f7dbe5243f5c3f8e4
A242490JR <A242490JR@rvt.lv>
2025-06-09 11:39:59 +0300

    Added more files, now read functionality is working

--- main/zippy.py
+++ main/zippy.py
@@ -1,5 +1,26 @@
-version = 'test 1.0'
+def main():
+    version = 'test 1.0'
 
-print("Zippy archiver")
-print("Version:", version)
+    # ---------------------------------------------- #
 
+    import os
+
+    # ---------------------------------------------- #
+
+    print("Zippy archiver")
+    print("Version:", version)
+
+
+    directory = os.listdir("./storage")
+    if len(directory) == 0:
+        print("\n\nFound nothing in /storage folder, please add items to /storage folder and try again.\n")
+        return
+    else:
+        print("\n\nFound these items in /storage folder:")
+        print("-------------------------------------")
+
+    for i in range(len(directory)):
+        if i != len(directory) - 1:
+            print(directory[i] + "[" + str(i + 1) + "]", end=", ")
+        else:
+            print(directory[i] + "[" + str(i + 1) + "]")
---
+++ run.py
@@ -0,0 +1,3 @@
+from main.zippy import main
+
+main()
